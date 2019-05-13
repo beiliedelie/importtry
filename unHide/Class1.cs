806f7063ad67e63df4cade3a5103a1b333d482bd
beilie <1043410556@qq.com>
2019-05-13 19:58:35 +0800

    add file

---
+++ unHide/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+namespace unHide
+{
+    [Transaction(TransactionMode.Manual)]
+    public class UnHideFamily : IExternalCommand
+    {
+        Document doc;
+        Application app;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+
+            doc = uiDoc.Document;
+
+            FilteredElementCollector hideEleCollector = new FilteredElementCollector(doc).WhereElementIsNotElementType();
+            List<ElementId> hideElementIds = new List<ElementId>();
+            foreach (var item in hideEleCollector)
+            {
+                if (item.CanBeHidden(doc.ActiveView) && item.IsHidden(doc.ActiveView))
+                {
+                    hideElementIds.Add(item.Id);
+                }
+            }
+            Transaction trans = new Transaction(doc, "显示");
+            trans.Start();
+            doc.ActiveView.UnhideElements(hideElementIds);
+            trans.Commit();
+
+            return Result.Succeeded;
+
+        }
+    }
+}
